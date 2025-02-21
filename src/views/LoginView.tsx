ada7903ecae6c1af2a9f20398482374a8a9f8815
Alew <xalejandrow@gmail.com>
2025-02-21 01:05:37 -0300

    Creando el Layout Principal para Autenticacion

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -3,8 +3,6 @@ import { Link } from "react-router-dom"
 export default function LoginView() {
   return (
         <>
-            <div>LoginView</div>
-
             <nav>
                 <Link to="/auth/register">
                     ¿No tienes cuenta? Crea una aquí
