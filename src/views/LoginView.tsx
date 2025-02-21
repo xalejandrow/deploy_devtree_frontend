bc41258959a80c46939d86f2d0a10e5d006c2bf7
Alew <xalejandrow@gmail.com>
2025-02-21 00:52:11 -0300

    Navegando entre páginas con React Router

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom"
+
 export default function LoginView() {
   return (
-    <div>
-      LoginView
-    </div>
+        <>
+            <div>LoginView</div>
+
+            <nav>
+                <Link to="/auth/register">
+                    ¿No tienes cuenta? Crea una aquí
+                </Link>
+            </nav>
+        </>
   )
 }
--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+
 export default function RegisterView() {
   return (
-    <div>
-      RegisterView
-    </div>
+    <>
+            <div>RegisterView</div>
+
+            <nav>
+                <Link to="/auth/login">
+                    ¿Ya tienes una cuenta? Inicia Sesión
+                </Link>
+            </nav>
+        </>
   )
 }
