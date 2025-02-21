0a9fd004c0541b6579a02db4433f957ee30820d2
Alew <xalejandrow@gmail.com>
2025-02-21 00:10:45 -0300

    Instalando tailwindcss y borrando Código innecesario

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const holaMundo = "Hola Mundo desde TSX"
 
   return (
     <>
-      { holaMundo }
+      <h1>Hola Mundo</h1>
     </>
   )
 }
