c98abb2fcabd97f2d0c21bbe49a8bc98fdd1da4f
Alew <xalejandrow@gmail.com>
2025-02-21 15:33:15 -0300

    Agregando Estilos a la Navegación

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -3,9 +3,13 @@ import { Link } from "react-router-dom"
 export default function LoginView() {
   return (
         <>
-            <nav>
-                <Link to="/auth/register">
-                    ¿No tienes cuenta? Crea una aquí
+            <div className="text-4xl text-white font-bold">Iniciar Sesión</div>
+
+            <nav className="mt-10">
+                <Link 
+                    className="text-center text-white text-lg block"
+                    to="/auth/register"
+                >¿No tienes cuenta? Crea una aquí
                 </Link>
             </nav>
         </>
--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -1,15 +1,17 @@
 import { Link } from "react-router-dom";
 
 export default function RegisterView() {
-  return (
-    <>
-            <div>RegisterView</div>
+    return (
+            <>
+                <div className="text-4xl text-white font-bold">Crear Cuenta</div>
 
-            <nav>
-                <Link to="/auth/login">
-                    ¿Ya tienes una cuenta? Inicia Sesión
-                </Link>
-            </nav>
-        </>
-  )
+                <nav className="mt-10">
+                    <Link 
+                        className="text-center text-white text-lg block"
+                        to="/auth/login"
+                    >¿Ya tienes una cuenta? Inicia Sesión
+                    </Link>
+                </nav>
+            </>
+    )
 }
