4e9725733695ea3579705ba867d3f30ddcd045e3
Alew <xalejandrow@gmail.com>
2025-02-24 18:25:07 -0300

    ¿Donde almacenar el JWT? Cookies o LocalStorage

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -21,7 +21,8 @@ export default function LoginView() {
     const handleLogin = async (formData : LoginForm) => {
         try {
             const { data } = await api.post(`/auth/login`, formData);
-            toast.success(data);
+            localStorage.setItem('AUTH_TOKEN', data);
+            // toast.success(data);
         } catch (error) {
             if(isAxiosError(error) && error.response) {
                 toast.error(error.response?.data.error);
