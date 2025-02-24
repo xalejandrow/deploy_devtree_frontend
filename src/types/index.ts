c9b017aff76b8eb65ee433812d9121cff05ae2a6
Alew <xalejandrow@gmail.com>
2025-02-24 17:12:14 -0300

    Añadiendo el Formulario y useForm

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,10 +2,13 @@ export type User = {
     handle: string
     name: string
     email: string
-    password: string
 }
 
 export type RegisterForm = Pick<User, 'name' | 'email' | 'handle'> & {
     password: string
     password_confirmation: string
+}
+
+export type LoginForm = Pick<User, 'email'> & {
+    password: string
 }
--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,17 +1,85 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import ErrorMessage from "../components/ErrorMessage";
+import { LoginForm } from "../types";
 
 export default function LoginView() {
-  return (
-        <>
-            <div className="text-4xl text-white font-bold">Iniciar Sesión</div>
+  const initialValues : LoginForm = {
+    email: "",
+    password: "",
+  };
 
-            <nav className="mt-10">
-                <Link 
-                    className="text-center text-white text-lg block"
-                    to="/auth/register"
-                >¿No tienes cuenta? Crea una aquí
-                </Link>
-            </nav>
-        </>
-  )
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ defaultValues: initialValues });
+
+    const handleLogin = (formData : LoginForm) => {
+        console.log(formData);
+    };
+
+  return (
+    <>
+      <h1 className="text-4xl text-white font-bold">Iniciar Sesión</h1>
+
+      <form
+        onSubmit={handleSubmit(handleLogin)}
+        className="bg-white px-5 py-20 rounded-lg space-y-10 mt-10"
+        noValidate
+      >
+        <div className="grid grid-cols-1 space-y-3">
+          <label htmlFor="email" className="text-2xl text-slate-500">
+            E-mail
+          </label>
+          <input
+            id="email"
+            type="email"
+            placeholder="Email de Registro"
+            className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+            {...register("email", {
+              required: "El Email es obligatorio",
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: "E-mail no válido",
+              },
+            })}
+          />
+          {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
+        </div>
+        <div className="grid grid-cols-1 space-y-3">
+          <label htmlFor="password" className="text-2xl text-slate-500">
+            Password
+          </label>
+          <input
+            id="password"
+            type="password"
+            placeholder="Password de Registro"
+            className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+            {...register("password", {
+              required: "El Password es obligatorio",
+            })}
+          />
+          {errors.password && (
+            <ErrorMessage>{errors.password.message}</ErrorMessage>
+          )}
+        </div>
+
+        <input
+          type="submit"
+          className="bg-cyan-400 p-3 text-lg w-full uppercase text-slate-600 rounded-lg font-bold cursor-pointer"
+          value="Iniciar Sesión"
+        />
+      </form>
+
+      <nav className="mt-10">
+        <Link
+          className="text-center text-white text-lg block"
+          to="/auth/register"
+        >
+          ¿No tienes cuenta? Crea una aquí
+        </Link>
+      </nav>
+    </>
+  );
 }
