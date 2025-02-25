3d904df5822c46414df942bd80795d1358a08858
Alew <xalejandrow@gmail.com>
2025-02-25 12:12:00 -0300

    Enviando la petición con el JWT en React Query

--- src/layouts/api/DevTreeAPI.ts
+++ src/layouts/api/DevTreeAPI.ts
@@ -2,11 +2,19 @@ import { isAxiosError } from "axios";
 import api from "../../config/axios";
 
 export async function getUser() {
+
+        const token = localStorage.getItem('AUTH_TOKEN');
+
         try {
             // const { data } = await api.get(`/user`);
-            const { data } = await api(`/user`); // This is the same as the line above
-            console.log(data);
-            // return data;
+            const { data } = await api(`/user`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+                    
+            }); // This is the same as the line above
+            // console.log(data);
+            return data;
         } catch (error) {
             if(isAxiosError(error) && error.response) {
                throw new Error(error.response.data.error);
