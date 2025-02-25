0de902bea6372b392831f673503c0d261978698b
Alew <xalejandrow@gmail.com>
2025-02-25 14:03:16 -0300

    Proteger el Panel de Administración si un usuario no esta autenticado

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,24 +1,23 @@
 import { Link, Outlet } from "react-router-dom";
 import { Toaster } from "sonner";
 import { useQuery } from "@tanstack/react-query"
+import { Navigate } from "react-router-dom";
 import NavigationTabs from "../components/NavigationTabs";
 import { getUser } from "./api/DevTreeAPI";
 
 export default function AppLayout() {
 
-    const { data, isLoading, error, isError } = useQuery({
+    const { data, isLoading, isError } = useQuery({
        queryFn : getUser,
        queryKey: ['user'],
        retry: 1,
        refetchOnWindowFocus: false,
     })
 
-    console.log(data);
-    console.log(isLoading);
-    console.log(isError);
-    console.log(error?.message);
-    
-    
+    if(isLoading) return 'Cargando...'
+
+    if(isError) return <Navigate to="/auth/login" />
+
 
     return (
         <>
