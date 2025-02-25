56415c7fd0be6c4be1b7c9eeaa53bb110a8ac31c
Alew <xalejandrow@gmail.com>
2025-02-25 10:12:02 -0300

    Creando el Formulario y Diferencias entre Autenticación y Autorización

--- src/views/ProfileView.tsx
+++ src/views/ProfileView.tsx
@@ -1,7 +1,53 @@
+
+
 export default function ProfileView() {
-  return (
-    <div>
-      ProfileView
-    </div>
-  )
+
+    return (
+        <form 
+            className="bg-white p-10 rounded-lg space-y-5"
+            onSubmit={() => {}}
+        >
+            <legend className="text-2xl text-slate-800 text-center">Editar Información</legend>
+            <div className="grid grid-cols-1 gap-2">
+                <label
+                    htmlFor="handle"
+                >Handle:</label>
+                <input
+                    type="text"
+                    className="border-none bg-slate-100 rounded-lg p-2"
+                    placeholder="handle o Nombre de Usuario"
+                />
+            </div>
+
+            <div className="grid grid-cols-1 gap-2">
+                <label
+                    htmlFor="description"
+                >Descripción:</label>
+                <textarea
+                    className="border-none bg-slate-100 rounded-lg p-2"
+                    placeholder="Tu Descripción"
+                />
+            </div>
+
+            <div className="grid grid-cols-1 gap-2">
+                <label
+                    htmlFor="handle"
+                >Imagen:</label>
+                <input
+                    id="image"
+                    type="file"
+                    name="handle"
+                    className="border-none bg-slate-100 rounded-lg p-2"
+                    accept="image/*"
+                    onChange={ () => {} }
+                />
+            </div>
+
+            <input
+                type="submit"
+                className="bg-cyan-400 p-2 text-lg w-full uppercase text-slate-600 rounded-lg font-bold cursor-pointer"
+                value='Guardar Cambios'
+            />
+        </form>
+    )
 }
