4c02703b0f7cdc966d93d796e3d581934d8ed566
Alew <xalejandrow@gmail.com>
2025-02-26 18:01:37 -0300

    Detectando si una URL es válida antes de habilitarla

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,12 @@
 export function classNames(...classes : string[]) {
     return classes.filter(Boolean).join(' ')
 }
+
+export function isValidUrl(url: string) {
+    try {
+        new URL(url);
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
--- src/views/LinkTreeView.tsx
+++ src/views/LinkTreeView.tsx
@@ -1,37 +1,47 @@
-import { useState } from "react"
-import { social } from "../data/social"
-import DevTreeInput from "../components/DevTreeInput"
+import { useState } from "react";
+import { social } from "../data/social";
+import DevTreeInput from "../components/DevTreeInput";
+import { isValidUrl } from "../utils";
+import { toast } from "sonner";
 
 export default function LinkTreeView() {
+  const [devTreeLinks, setDevTreeLinks] = useState(social);
 
-  const [devTreeLinks, setDevTreeLinks] = useState(social)
-
-  const handleUrlChange = (e : React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.value);
-    console.log(e.target.name);
-    const updatedLinks = devTreeLinks.map( link => link.name === e.target.name ? { ...link, url: e.target.value } : link )
+  const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // console.log(e.target.value);
+    // console.log(e.target.name);
+    const updatedLinks = devTreeLinks.map((link) =>
+      link.name === e.target.name ? { ...link, url: e.target.value } : link
+    );
     console.log(updatedLinks);
-    setDevTreeLinks(updatedLinks)
-  }
+    setDevTreeLinks(updatedLinks);
+  };
 
   const handleEnableLink = (socialNetwork: string) => {
-   const updatedLinks = devTreeLinks.map( link => link.name === socialNetwork ? { ...link, enabled: !link.enabled } : link )
-   console.log(updatedLinks)
-   setDevTreeLinks(updatedLinks) 
-  }
-  
+    const updatedLinks = devTreeLinks.map((link) => {
+      if (link.name === socialNetwork) {
+        if (isValidUrl(link.url)) {
+          return { ...link, enabled: !link.enabled };
+        } else {
+          toast.error("URL no válida");
+        }
+      }
+      return link;
+    });
+    console.log(updatedLinks);
+    setDevTreeLinks(updatedLinks);
+  };
 
   return (
     <div className="space-y-5">
-      {devTreeLinks.map( item => (
-        <DevTreeInput 
+      {devTreeLinks.map((item) => (
+        <DevTreeInput
           key={item.name}
           item={item}
           handleUrlChange={handleUrlChange}
           handleEnableLink={handleEnableLink}
         />
       ))}
-      
     </div>
-  )
+  );
 }
