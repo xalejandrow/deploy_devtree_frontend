2ada4428c61d502d5a41640526e25da9b154fb7c
Alew <xalejandrow@gmail.com>
2025-03-05 15:56:55 -0300

    Mostrando los enlaces habilitados en nuestro perfil

--- src/components/DevTree.tsx
+++ src/components/DevTree.tsx
@@ -1,13 +1,19 @@
 import { Link, Outlet } from "react-router-dom";
 import { Toaster } from "sonner";
 import NavigationTabs from "./NavigationTabs";
-import { User } from "../types";
+import { SocialNetwork, User } from "../types";
+import { useState } from "react";
 
 type DevTreeProps = {
     data: User
 }
 
 export default function DevTree({ data } : DevTreeProps) {
+
+    const [enableLinks, setEnableLinks] = useState<SocialNetwork[]>(JSON.parse(data.links).filter((item: SocialNetwork) => item.enabled));
+    
+    // console.log(enableLinks);
+    
   return (
     <>
             <header className="bg-slate-800 py-5">
@@ -50,6 +56,12 @@ export default function DevTree({ data } : DevTreeProps) {
                             }
 
                             <p className="text-center text-lg font-black text-white">{data.description}</p>
+
+                            <div className="mt-20 flex flex-col gap-5">
+                                {enableLinks.map((link) => (
+                                    <p>{link.url}</p>
+                                ))}
+                            </div>
                         </div>
                     </div>
                 </main>
--- src/views/LinkTreeView.tsx
+++ src/views/LinkTreeView.tsx
@@ -5,7 +5,7 @@ import DevTreeInput from "../components/DevTreeInput";
 import { isValidUrl } from "../utils";
 import { toast } from "sonner";
 import { updateProfile } from "../api/DevTreeAPI";
-import { User } from "../types";
+import { User, SocialNetwork } from "../types";
 
 export default function LinkTreeView() {
   const [devTreeLinks, setDevTreeLinks] = useState(social);
@@ -25,7 +25,7 @@ export default function LinkTreeView() {
 
   useEffect(() => {
     const updatedData = devTreeLinks.map(item => {
-        const userLinks = JSON.parse(user.links).find(link => link.name === item.name);
+        const userLinks = JSON.parse(user.links).find((link: SocialNetwork) => link.name === item.name);
         if (userLinks) {
           return { ...item, url: userLinks.url, enabled: userLinks.enabled };
         }
