c5be4df30e568278b565ef07cfa63600a03e2335
Alew <xalejandrow@gmail.com>
2025-03-06 19:09:14 -0300

    Creando un Componente re-utilizable para el Logo y Navegaciones

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,19 +1,21 @@
-import { Outlet } from "react-router-dom"
-import { Toaster } from "sonner"
+import { Outlet } from "react-router-dom";
+import { Toaster } from "sonner";
+import Logo from "../components/Logo";
 
 export default function AuthLayout() {
   return (
     <>
-        <div className="bg-slate-800 min-h-screen">
-            <div className="max-w-lg mx-auto pt-10 px-5">
-                <img src='/logo.svg' alt='Logotipo Devtree' />
-                <div className="py-10">
-                    <Outlet />
-                </div>
-            </div>
-        </div>
+      <div className="bg-slate-800 min-h-screen">
+			<div className="max-w-lg mx-auto pt-10 px-5">
+				<Logo />
+				
+				<div className="py-10">
+					<Outlet />
+				</div>
+			</div>
+      </div>
 
-        <Toaster position="top-right"/>
+      <Toaster position="top-right" />
     </>
-  )
+  );
 }
