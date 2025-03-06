e7ac86a1b45d942fc7ad2a24458b6fa16992b7e3
Alew <xalejandrow@gmail.com>
2025-03-06 19:18:06 -0300

    Primeros pasos con la página principal

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -4,6 +4,20 @@ export default function HomeView() {
   return (
     <>
         <Header />
+
+        <main className="bg-gray-100 py-10 min-h-screen">
+            <div className="max-w-5xl mx-auto mt-10">
+                <div className="lg:w-1/2 px-10 lg:p-0 space-y-6">
+                    <h1 className="text-6xl font-black">
+                        Todas tus <span className="text-cyan-400">Redes Sociales </span> en un enlace
+                    </h1>
+
+                    <p className="text-slate-800 text-xl">Únete a más de 200 mil 
+                        developers compartiendo sus redes sociales, comparte tu perfil de 
+                        TikTok, Facebook, Instagram, YouTube, Github y más</p>
+                </div>
+            </div>
+        </main>
     </>
   )
 }
