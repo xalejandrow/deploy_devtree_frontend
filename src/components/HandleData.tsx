c667752704e89a08ae358fc34a7b9323e6308382
Alew <xalejandrow@gmail.com>
2025-03-06 18:01:55 -0300

    Mostrando Handle, Imagen y Descripción

--- src/components/HandleData.tsx
+++ src/components/HandleData.tsx
@@ -1,8 +1,16 @@
+import { UserHandle } from "../types"
 
-export default function HandleData() {
+type HandleDataProps = {
+  data: UserHandle
+}
+export default function HandleData({data}: HandleDataProps) {
   return (
-    <div>
-      HandleData
+    <div className="space-y-6 text-white">
+        <p className="text-5xl text-center font-black">{data.handle}</p>
+        {data.image && <img src={data.image} alt={data.handle} className="max-w-[250px] mx-auto" />}
+        <p className="text-lg text-center font-bold">{data.description}</p>
+        
+      
     </div>
   )
 }
