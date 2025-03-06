2ced24a104c2f8571f76c2b15b0db47c25e4a839
Alew <xalejandrow@gmail.com>
2025-03-06 15:13:03 -0300

    Añadiendo la funcionalidad de Drag

--- src/components/DevTreeLink.tsx
+++ src/components/DevTreeLink.tsx
@@ -1,3 +1,5 @@
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 import { SocialNetwork } from "../types"
 
 type DevTreeLinkProps = {
@@ -5,8 +7,23 @@ type DevTreeLinkProps = {
 }
 
 export default function DevTreeLink({ link }: DevTreeLinkProps) {
+
+  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ 
+    id: link.id 
+  });
+
+  const style = {
+	transform: CSS.Transform.toString(transform),
+	transition
+  }
   return (
-    <li className="bg-white px-5 py-2 flex items-center gap-5 rounded-lg">
+    <li 
+		ref={setNodeRef}
+		style={style}
+		className="bg-white px-5 py-2 flex items-center gap-5 rounded-lg"
+		{...attributes}
+		{...listeners}	
+	>
       <div
         className="w-12 h-12 bg-cover"
         style={{ backgroundImage: `url(/social/icon_${link.name}.svg)` }}
