a4c0ad4e7f52a539007372084c84fdc907a35738
Alew <xalejandrow@gmail.com>
2025-03-06 18:17:05 -0300

    Mostrando los enlaces activos del Usuario

--- src/components/HandleData.tsx
+++ src/components/HandleData.tsx
@@ -1,16 +1,42 @@
-import { UserHandle } from "../types"
+import { SocialNetwork, UserHandle } from "../types";
 
 type HandleDataProps = {
-  data: UserHandle
-}
-export default function HandleData({data}: HandleDataProps) {
+  data: UserHandle;
+};
+export default function HandleData({ data }: HandleDataProps) {
+  const links: SocialNetwork[] = JSON.parse(data.links).filter(
+    (link: SocialNetwork) => link.enabled
+  );
+//   console.log(links);
+
   return (
     <div className="space-y-6 text-white">
-        <p className="text-5xl text-center font-black">{data.handle}</p>
-        {data.image && <img src={data.image} alt={data.handle} className="max-w-[250px] mx-auto" />}
-        <p className="text-lg text-center font-bold">{data.description}</p>
-        
-      
+      <p className="text-5xl text-center font-black">{data.handle}</p>
+      {data.image && (
+        <img
+          src={data.image}
+          alt={data.handle}
+          className="max-w-[250px] mx-auto"
+        />
+      )}
+      <p className="text-lg text-center font-bold">{data.description}</p>
+      <div className="mt-20 flex flex-col gap-6">
+        {links.length > 0 ?  
+        links.map((link: SocialNetwork) => (
+          <a
+            key={link.name}
+            className="bg-white px-5 py-2 flex items-center gap-5 rounded-lg"
+            href={link.url}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <img src={`/social/icon_${link.name}.svg`} alt="imagen red social" className="w-12"/>
+            <p className="text-black capitalize font-bold text-lg">Visita mi: {link.name}</p>
+          </a>
+        ))
+        : <p className="text-center">No hay enlaces en este perfil</p>  }
+      </div>
+
     </div>
-  )
+  );
 }
