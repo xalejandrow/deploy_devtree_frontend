ae2905f9ea0fb94ac021710c8917d2ccb82d6934
Alew <xalejandrow@gmail.com>
2025-03-06 20:28:06 -0300

    Cerrando sesión del usuario

--- src/components/nav/AdminNavigation.tsx
+++ src/components/nav/AdminNavigation.tsx
@@ -1,10 +1,19 @@
+import { useQueryClient } from "@tanstack/react-query";
+
 export default function AdminNavigation() {
-  return (
-    <button
-      className=" bg-lime-500 p-2 text-slate-800 uppercase font-black text-xs rounded-lg cursor-pointer"
-      onClick={() => {}}
-    >
-      Cerrar Sesión
-    </button>
-  );
+	const queryClient = useQueryClient();
+
+	const logout = () => {
+		localStorage.removeItem('AUTH_TOKEN');
+		queryClient.invalidateQueries({ queryKey: ['user']});
+	};
+
+	return (
+		<button
+		className=" bg-lime-500 p-2 text-slate-800 uppercase font-black text-xs rounded-lg cursor-pointer"
+		onClick={logout}
+		>
+		Cerrar Sesión
+		</button>
+	);
 }
