f366d0f74e84e5d8236b9fbbdab9344df66722e7
Alew <xalejandrow@gmail.com>
2025-03-06 16:26:35 -0300

    Creando la página para Compartir tus enlaces

--- src/router.tsx
+++ src/router.tsx
@@ -5,6 +5,7 @@ import AuthLayout from "./layouts/AuthLayout";
 import AppLayout from "./layouts/AppLayout";
 import LinkTreeView from "./views/LinkTreeView";
 import ProfileView from "./views/ProfileView";
+import HandleView from "./views/HandleView";
 
 export default function Router() {
 
@@ -20,6 +21,11 @@ export default function Router() {
           <Route index={true} element={<LinkTreeView />} />
           <Route path="profile" element={<ProfileView />} />
         </Route>
+
+        <Route path="/:handle" element={<AuthLayout />} >
+          <Route element={<HandleView />} index={true} />
+        </Route>
+
       </Routes>
     </BrowserRouter>
   );
---
+++ src/views/HandleView.tsx
@@ -0,0 +1,8 @@
+
+export default function HandleView() {
+  return (
+    <div>
+      HandleView
+    </div>
+  )
+}
